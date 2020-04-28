17fed7cf705f505c900b811d84d49faab7809d59
Liv.R <livreinhold@gmail.com>
2020-04-28 11:14:05 +0200

    Created GET request to get one user

--- Server.js
+++ Server.js
@@ -11,6 +11,7 @@ const pool = new Pool({
 });
 
 
+/** Get all users */
 app.get("/", (req, res) => {
     pool
       .query('SELECT * FROM users;')
@@ -18,6 +19,14 @@ app.get("/", (req, res) => {
       .catch(e => res.sendStatus(404)); 
   });
   
+  /** Get all users with "id" */
+  app.get("/:id", (req, res) => {
+    const { id } = req.params; 
+    pool
+      .query('SELECT * FROM users WHERE id=$1;', [id])
+      .then(data => res.json(data)) 
+      .catch(e => res.sendStatus(404)); 
+  });
 
 
 
