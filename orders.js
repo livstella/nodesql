5874e9f88a2edeef784b8f80016733b4f25a41ff
Liv.R <livreinhold@gmail.com>
2020-04-28 14:03:58 +0200

    Added GET to get all order

---
+++ orders.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const { Pool } = require('pg');
+var bodyParser = require('body-parser')
+
+
+const pool = new Pool({
+ user: 'kmjlacfk',
+ host: 'balarama.db.elephantsql.com',
+ database: 'kmjlacfk',
+ password: 'JFHaVsipVYb9NXs7DOdVxPLCq9cGVWvI',
+ port: 5432,
+});
+
+
+
+/** Get all orders */
+  app.get("/", (req, res) => {
+    pool
+      .query('SELECT * FROM orders;')
+      .then(data => res.json(data)) 
+      .catch(e => res.sendStatus(404)); 
+  });
+
+  
+  
+  
+  
+  app.listen('3000', () => console.log('connected'));
